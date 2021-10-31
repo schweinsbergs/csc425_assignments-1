79ec6ce3112a6145ed52795d1973743079221053
schweinsbergs <schweinsbergs@csc4252021.ssdt.berea.edu>
2021-10-31 17:56:45 -0400

    added some code

--- coding.freeList/freeList.cc
+++ coding.freeList/freeList.cc
@@ -1,21 +1,51 @@
 #include "freeList.h"
 
 freeList::freeList( long int*inRAM, int size ) {
+
+int (*x)[size]; // point to an array that's a constructed size?
+
 }
+
+
 
 long int*
 freeList::reserve_space( int size ) {
   long int* loc = NULL;
-
   return loc;
+
+loc = loc + 2; // adding for overhead? 
+
+
+
 }
+
+
+
 
 void
 freeList::free_space( long int* location ) {
+
+long int* next = 0
+
+*pointer_value = loc; 
+*loc + size =  next; // the size of the next free space is the location of the pointer plus the size of the space given
+next = next + 2; // and add 2 for the overhead 
+
+
+
 }
+
+
+
 
 void
 freeList::coalesce_forward() {
+
+if size == 0 &&  next_size == 0; 
+// point to the head of the first and add the sizes...
+
+
+
 }
 
 // print() is given to you. It traverses the free list and prints out what is there.
